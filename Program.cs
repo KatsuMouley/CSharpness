7d78af6994a0c16b0982130eddafa82c8b388620
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-08-09 12:20:50 -0300

    First code in c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Project1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira a sua idade: ");
+        var idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Você tem "+idade+" anos");
+
+    }
+}
